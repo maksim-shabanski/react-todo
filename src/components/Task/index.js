b0055b7a2cf0963e13ac53b8271c4b95a11e5476
Maksim Shabanski <shabanskij.maksim@gmail.com>
2020-01-16 15:57:37 +0300

    fix: prevent edit text of the task when task is completed

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -26,7 +26,12 @@ const Task = ({
           onClick={() => completeTask(id)}
         />
       </div>
-      <TaskText id={id} text={text} updateTaskText={updateTaskText} />
+      <TaskText
+        id={id}
+        text={text}
+        updateTaskText={updateTaskText}
+        isCompleted={isCompleted}
+      />
       <Button onClick={() => removeTask(id)}>
         <svg className={`${prefix}__close`} viewBox="0 0 20 20">
           <polygon points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644" />
--- src/components/TaskText/index.js
+++ src/components/TaskText/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TaskText = ({ id, text, updateTaskText }) => {
+const TaskText = ({ id, text, updateTaskText, isCompleted }) => {
+  const canEditTaskText = !isCompleted;
+
   const handleFocusLost = ({ target }) => {
     const updatedText = target.innerText.trim();
 
@@ -25,7 +27,7 @@ const TaskText = ({ id, text, updateTaskText }) => {
       tabIndex="0"
       onBlur={handleFocusLost}
       onKeyPress={handleKeyPress}
-      contentEditable
+      contentEditable={canEditTaskText}
       suppressContentEditableWarning
     >
       {text}
@@ -37,6 +39,7 @@ TaskText.propTypes = {
   id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   updateTaskText: PropTypes.func.isRequired,
+  isCompleted: PropTypes.bool.isRequired,
 };
 
 export default TaskText;
